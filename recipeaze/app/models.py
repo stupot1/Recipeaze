d2d2960241964ca68a17222304cbb5d71f375464
stupot1 <64636440+stupot1@users.noreply.github.com>
2021-11-21 12:23:03 +0000

    Created Recipe, Ingredient, Tag Model and DB

--- recipeaze/app/models.py
+++ recipeaze/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class Recipe(models.Model):
+    recipe_text = models.TextField()
+
+class Ingredient(models.Model):
+    ingredient_text = models.TextField()
+
+class Tag(models.Model):
+    tag_text = models.TextField()
